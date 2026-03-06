5fb4a61bdd57924e7cdd36c6bf7acddca0d62d42
roprogramer91 <roprogramer91@gmail.com>
2026-03-06 20:33:55 -0300

    fix prisma import for railway esm

--- backend/prismaClient.js
+++ backend/prismaClient.js
@@ -1,12 +1,7 @@
-import { PrismaClient } from "@prisma/client";
-import "dotenv/config";
+import pkg from "@prisma/client";
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
+const { PrismaClient } = pkg;
+
+const prisma = new PrismaClient();
 
 export default prisma;
